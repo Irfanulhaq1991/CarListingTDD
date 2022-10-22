13f9a8941ed9680500f7f8932a2acaab1425363d
Irfan <irfan@Irfans-MacBook-Air.local>
2022-10-22 11:12:46 +0500

    Manufacturers: ViewModel to call fetchManufacturersUsecase failing test is failing for the right reason.

--- app/src/test/java/com/irfan/auto1/manufacturers/ManufacturersViewModelShould.kt
+++ app/src/test/java/com/irfan/auto1/manufacturers/ManufacturersViewModelShould.kt
@@ -2,6 +2,7 @@ package com.irfan.auto1.manufacturers
 
 import com.irfan.auto1.BaseTest
 import com.irfan.auto1.FetchManufacturersUseCase
+import com.irfan.auto1.ManufacturersViewModel
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.verify
 import org.junit.Before
@@ -11,14 +12,17 @@ class ManufacturersViewModelShould : BaseTest() {
 
     @RelaxedMockK
     private lateinit var fetchManufactureUseCase: FetchManufacturersUseCase
+    private lateinit var viewModel: ManufacturersViewModel
 
     @Before
     override fun setup() {
         super.setup()
+        viewModel = ManufacturersViewModel()
     }
 
     @Test
     fun fetchManufacturers() {
+        viewModel.fetchManufacturers()
         verify { fetchManufactureUseCase }
     }
 }
